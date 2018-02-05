ca1a14a1b5409216c871d07f2579a3c132388129
Patricia Richardson <triciadrich@gmail.com>
2018-02-05 10:33:21 -0800

    added main method that prints hello world

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -1,4 +1,11 @@
 package hello;
 
+/**
+ * Created by Patricia Richardson 2/5/2018
+ */
+
 public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+    }
 }
